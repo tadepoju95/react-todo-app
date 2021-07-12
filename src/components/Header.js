ecd9030feca65629803fb7665f65e2d736286c6f
Taiwo Adepoju <tadepoju95@gmail.com>
2021-07-11 20:56:37 -0500

    Modified files in project folder.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './main.css';
-import { Container, Row, Col, Nav, Jumbotron } from 'react-bootstrap'
+import { Container, Row } from 'react-bootstrap'
 import GoogleAuth from './GoogleAuth';
 
 
--- src/components/ListOfTodos.js
+++ src/components/ListOfTodos.js
@@ -1,24 +1,33 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import{ fetchAllTodos } from '../actions';
+import{ fetchAllTodos, editTodo } from '../actions';
 import { Container } from 'react-bootstrap'
 
 
 class ListOfTodos extends React.Component {
+	state = { value: "", removeReadOnly: true };
 
 	componentDidMount() {
 		this.props.fetchAllTodos();
 	}
 
+	handleChange = e => {
+    	this.setState({value: e.target.value});
+  	}
+
+  	handleReadOnly() {
+  		this.setState({removeReadOnly: !this.state.removeReadOnly});
+  	}
+  	
 	renderList () {
 		if (this.props.isSignedIn) {
 		return this.props.allTodos.map((eachTodo) => {
 			return (
 				<Container key={eachTodo.id}>
 					<div className="input-group mb-3 list">
-  						<input type="text" className="form-control" value={eachTodo.inputValue} aria-label="Recipient's username" aria-describedby="button-addon2" />
+  						<input type="text" className="form-control" value={eachTodo.inputValue} onChange={this.handleChange} aria-label="Recipient's username" aria-describedby="button-addon2" readOnly={this.state.removeReadOnly} />
   						<div className="input-group-append">
-    						<button className="btn btn-outline-secondary" type="button" id="button-addon2">Edit</button>
+    						<button className="btn btn-outline-secondary" type="button" id="button-addon2"  onClick={() => this.handleReadOnly()}>Edit</button>
     						<button className="btn btn-outline-secondary" type="button" id="button-addon2">Update</button>
     						<button className="btn btn-outline-secondary" type="button" id="button-addon2">Delete</button>
  						</div>
@@ -47,4 +56,4 @@ const mapStateToProps = state => {
 }
 
 
-export default connect(mapStateToProps, { fetchAllTodos })(ListOfTodos);
+export default connect(mapStateToProps, { fetchAllTodos, editTodo })(ListOfTodos);
